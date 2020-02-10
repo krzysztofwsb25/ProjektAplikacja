3fdedfeada39d9c804efaa5b7835cf69e4531090
krzysztofwsb25 <56195886+krzysztofwsb25@users.noreply.github.com>
2020-02-10 15:35:17 +0100

    Add files via upload

---
+++ EatexProjekt/app/src/main/java/com/example/eatexprojekt/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.eatexprojekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenFindUs();
+            }
+        });
+    }
+    public void OpenFindUs() {
+    Intent intent = new Intent(this, FindUs.class);
+    startActivity(intent);
+
+
+    }
+
+}
